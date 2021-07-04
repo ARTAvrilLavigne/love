abc5eccc7e7e5d114963428619199cd992a2594a
Avril Lavigne <952727451@qq.com>
2021-07-04 22:39:57 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,20 @@ var config = {
 //         "日月与卿",
 //         "日为朝，月为暮",
 //         "卿为朝朝暮暮",
-	    "两情若是久长时",
-	    "又岂在朝朝暮暮",
-	    "when can I see you again?"
+	    "Hey！Siri",
+	    "How do u say goodbye to someone you can't imagine living without？",
+	    "The question is too difficult to answer.",
+	    "What would you say？",
+	    "I didn't say goodbye",
+	    "I didn't say anyting",
+	    "I just walked away",
+	    "Are you still feeling sad？",
+	    "Of course",
+	    "But you know",
+	    "Not every encounter has an end",
+	    "But each meeting carries some meanings",
+	    "Yes，some people are there to help you grow",
+	    "I wish you happiness",
     ],
 
     // 年-月-日T时时:分分:秒秒
